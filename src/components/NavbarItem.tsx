f05593a4c11782b51fa41400a45aab6856a83410
Paulo Brito <ext.paulo.brito@farfetch.com>
2024-03-15 15:15:13 +0000

    fix: navbar error deploy

--- src/components/NavbarItem.tsx
+++ src/components/NavbarItem.tsx
@@ -1,10 +1,43 @@
 'use client'
+// import Link from "next/link";
+// import { useSearchParams } from "next/navigation";
+
+// export default function NavbarItem({ title, param }: any) {
+//    const searchParams = useSearchParams()
+//    const genre = searchParams.get('genre')
+//    return (
+//       <div>
+//          <Link
+//             className={`hover:text-amber-600 font-semibold
+//             ${genre === param
+//                &&
+//                'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
+//                }`}
+//             href={`/?genre=${param}`}>{title}</Link>
+//       </div>
+//    )
+// }
+
+
+
+// NavbarItem.js
+import React from 'react';
 import Link from "next/link";
+import { Suspense } from 'react'; // Import Suspense
 import { useSearchParams } from "next/navigation";
 
-export default function NavbarItem({ title, param }) {
-   const searchParams = useSearchParams()
-   const genre = searchParams.get('genre')
+export default function NavbarItem({ title, param }: any) {
+   return (
+      <Suspense fallback={<div>Loading...</div>}>
+         <NavbarItemContent title={title} param={param} />
+      </Suspense>
+   );
+}
+
+function NavbarItemContent({ title, param }: any) {
+   const searchParams = useSearchParams();
+   const genre = searchParams.get('genre');
+
    return (
       <div>
          <Link
@@ -13,7 +46,10 @@ export default function NavbarItem({ title, param }) {
                &&
                'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
                }`}
-            href={`/?genre=${param}`}>{title}</Link>
+            href={`/?genre=${param}`}
+         >
+            {title}
+         </Link>
       </div>
-   )
+   );
 }
